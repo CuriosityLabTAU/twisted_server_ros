8561e8bf0333caa7a0eb8088efa05a9d34d7eb69
Hae Won Park <haewon@mit.edu>
2018-04-08 02:43:55 -0400

    add scripts for study execution

---
+++ scripts/send_msg_to_game.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+import json
+import rospy
+from std_msgs.msg import String
+import sys,os
+from time import sleep
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+json_file = "participant_info.json"
+
+
+def main(info):
+
+    #print info
+    rospy.init_node('GameCommand', anonymous=False)
+    publisher = rospy.Publisher('/to_twisted', String, queue_size=1)
+    sleep(0.5)
+
+    f = open(json_file)
+    info_dict = json.loads(f.read())
+
+    msg = "pid:"+info[0]+",condition:"+info_dict[info[0]]['condition']+",world:w"+info[1]+","+info[2]
+    #print msg
+
+    publisher.publish(msg)
+
+    print "Sent "+info[2]+" command for "+info[0]+" Session " + info[1]
+    print
+
+    try:
+        while True:
+            print "Enter 'start' or 'continue'"
+            command = raw_input(">> ")
+
+            if command == 'start' or command == 'continue':
+                msg = "pid:" + info[0] + ",condition:" + info_dict[info[0]]['condition'] + ",world:w" + info[1] + "," + \
+                  command
+                publisher.publish(msg)
+
+                print "Sent " + command + " command for " + info[0] + " Session " + info[1]
+                print
+            else:
+                print "wrong commang"
+                print
+
+    except KeyboardInterrupt:
+        pass
+
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ startROS.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+killall rosmaster
+killall roslaunch
+pkill -9 python
+sleep 0.5s
+
+WID=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)"| awk '{print $5}')
+xdotool windowfocus $WID
+xdotool key ctrl+shift+t
+wmctrl -i -a $WID
+
+sleep 1; xdotool type --delay 1 --clearmodifiers "roscore"; xdotool key Return;
+
+
+WID=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)"| awk '{print $5}')
+xdotool windowfocus $WID
+xdotool key ctrl+shift+t
+wmctrl -i -a $WID
+
+sleep 1; xdotool type --delay 1 --clearmodifiers "roslaunch rosbridge_server rosbridge_websocket.launch"; xdotool key Return;
+
+WID=$(xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)"| awk '{print $5}')
+xdotool windowfocus $WID
+xdotool key ctrl+shift+t
+wmctrl -i -a $WID
+
+sleep 1; xdotool type --delay 1 --clearmodifiers "python src/twisted_server_ros.py"; xdotool key Return;
