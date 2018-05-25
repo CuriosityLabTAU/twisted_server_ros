5571645e6b4cc4a4822eb37203da421978b72ba7
Hae Won Park <haewon@mit.edu>
2018-05-24 23:43:41 -0400

    jibo s8 script

--- scripts/send_msg_to_game.py
+++ scripts/send_msg_to_game.py
@@ -4,7 +4,8 @@ import rospy
 from std_msgs.msg import String
 import sys,os
 from time import sleep, time, strftime, gmtime
-from jibo_msgs.msg import JiboState
+from std_msgs.msg import Header 
+from jibo_msgs.msg import JiboState, JiboAction
 import threading
 
 abspath = os.path.abspath(__file__)
@@ -20,6 +21,12 @@ FREQ_THRESHOLD = 5
 STUCK_THRESHOLD = 5
 
 subscriber = None
+jibo_publisher = None
+
+s8_script=[
+        "<es name='Yawn_02' />  Oh, <participant-name>! Glad to see you. <es cat='happy'/> I'm <es name='Emoji_Beach'> going on a Summer vacation next week, <break size='0.3'/> so, sadly, this will be our last day</es> together. <es name='Sad_03' /> I definitely had so much fun getting to know you and playing and learning with you. <break size='0.5'/> and we got so many stickers together! Can you show me our sticker sheet?",
+        "Yay! Please keep it with you and remember me. <break size='1.0'/> Well, This calls for a celebration! <es name='electronic_house_music_dance_01'/> We made such a great team. <es name='affection_03'/> <es name='dont_understand_02'> Okay, I have to say good bye now, but I have a feeling</es> we will meet again soon. <es cat='happy'/> <break size='2.0'/> bye, <participant-name>. Thanks for being my friend. <break size='0.5'/> please come visit me at my place when you want to! I'll bake pizza <es name='Emoji_Pizza'/> and we will play more games together. <break size='1.0'/> Bye! <es name='goodbye_01'/> <es name='nap-to-sleep-01'/>"
+        ]
 
 def on_jibo_state_msg(data):
     global counter, motion_stuck_counter, prev_in_motion
@@ -57,13 +64,61 @@ def topic_freq():
             subscriber.unregister()
         subscriber = rospy.Subscriber('/jibo_state', JiboState, on_jibo_state_msg)
 
+def send_robot_tts_cmd(text):
+    msg = JiboAction()
+    # add header
+    msg.header = Header()
+    msg.header.stamp = rospy.Time.now()
+
+    msg.do_motion = False
+    msg.do_tts = True
+    msg.do_lookat = False
+    
+    msg.tts_text = text
+
+    jibo_publisher.publish(msg)
+        #rospy.loginfo(msg)
+        #print("\nSent TTS message: ", text)
+
+def send_robot_anim_transition_cmd(tran):
+
+    msg = JiboAction()
+    
+    # add header   
+    msg.header = Header()
+    
+    msg.header.stamp = rospy.Time.now()
+    msg.do_anim_transition = True
+
+    msg.anim_transition = tran
+    jibo_publisher.publish(msg)
+
+def send_robot_motion_cmd(command):
+    msg = JiboAction()
+    # add header
+    msg.header = Header()
+    msg.header.stamp = rospy.Time.now()
+
+    msg.do_motion = True
+    msg.do_tts = False
+    msg.do_lookat = False
+
+    msg.motion = command
+
+    jibo_publisher.publish(msg)
+
+def jibo_sleep():
+    send_robot_motion_cmd("Poses/Directional/Body_Look_Center_Down_01_01.keys")
+    send_robot_anim_transition_cmd(JiboAction.ANIMTRANS_KEEP_LASTFRAME)
+    send_robot_motion_cmd("Eye-Globals/open-to-close_01.keys")
 
 
 def main(info):
-    global subscriber
+    global subscriber, jibo_publisher
     #print info
     rospy.init_node('GameCommand', anonymous=False)
     publisher = rospy.Publisher('/to_twisted', String, queue_size=1)
+    jibo_publisher = rospy.Publisher('/jibo', JiboAction, queue_size=1)
     subscriber = rospy.Subscriber('/jibo_state', JiboState, on_jibo_state_msg)
 
     sleep(0.5)
@@ -94,6 +149,17 @@ def main(info):
                 command = "skip"
 
             if command == 'start' or command == 'continue' or command == 'skip':
+                #post-session
+                if info[1] == '8':
+                    send_robot_anim_transition_cmd(JiboAction.ANIMTRANS_RESET)
+                    pname = info_dict[info[0]]['name']
+                    send_robot_tts_cmd(s8_script[0].replace("<participant-name>",pname))
+                    sleep(35)
+                    send_robot_tts_cmd(s8_script[1].replace("<participant-name>",pname))
+                    continue
+
+                    
+                    
                 msg = {"pid": info[0],
                        "pname": info_dict[info[0]]['name'],
                        "robot": info_dict[info[0]]['robot'],
